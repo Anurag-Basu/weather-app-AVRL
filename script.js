c54bbf44285dec7a3eade2cca0b5822dc95d25c2
Anurag-Basu <basugupta312@gmail.com>
2021-12-21 22:18:32 +0530

    completed js file

--- script.js
+++ script.js
@@ -5,151 +5,104 @@ const cityEl = document.getElementById("city-list-name");
 const tbodyEl = document.getElementById("get-data");
 const cityName = ["London", "New York", "Los Angeles", "Las Vegas"];
 const getWeatherBtn = document.querySelector(".get-weather");
-const cities = document.querySelectorAll('.city');
+const cities = document.querySelectorAll(".city");
 const currentDate = new Date();
 
 function getWeather() {
-  
   let inputVal = input.value.trim();
-  
-    let index = -1;
-    cities.forEach((c , i) =>{
-        if(c.innerText.trim().toLowerCase() === inputVal.toLowerCase()){
-            index = i;
-            cities[index].classList.add("green");
-            return;
-        }
-    })
+  highlightCity(inputVal);
 
-  fetch(    `https://python3-dot-parul-arena-2.appspot.com/test?cityname=${inputVal}`  )
+  fetch(
+    `https://python3-dot-parul-arena-2.appspot.com/test?cityname=${inputVal}`
+  )
     .then((res) => res.json())
     .then((data) => {
       if (data.status === "failed") {
         return;
       } else {
-        const weatherDate = currentDate - new Date(data.date_and_time);
-        const weatherHours = Math.floor(weatherDate / 3600) %24;
-
-        tbodyEl.innerHTML += `
-            <tr>
-                <td>${inputVal}</td>
-                <td>${data.description}</td>
-                <td>${data.temp_in_celsius}</td>
-                <td>${data.pressure_in_hPa}</td>
-                <td>${weatherHours}</td>
-                <td> <button class="deleteBtn"> Delete </button> </td>
-            </tr>
-            `;
-
-
-
-        tbodyEl.addEventListener("click", (e) => onDeleteRow(e, inputVal));
+        fetchWeather(data, inputVal);
       }
     });
-    input.value ='';
+  input.value = "";
 }
 
-var index = -1;
-
-function onDeleteRow(e , value) {
-  if (!e.target.classList.contains("deleteBtn")) {
+function onDeleteRow(weather, value) {
+  let index = -1;
+  cities.forEach((c, i) => {
+    if (c.innerText.trim() === value) {
+      index = i;
+      return;
+    }
     return;
-  }
+  });
+  cities[index].classList.remove("green");
+  weather.remove();
+  console.log(index, "index");
+  return;
+}
 
-  
-  const btn = e.target;
-  console.log(e.target.closest('tr'))
-  btn.closest("tr").remove();
+function fetchAllCities() {
+  cities.forEach((c, i) => {
+    if (c.innerText.toLowerCase() === cityName[i].toLowerCase()) {
+      cities[i].classList.add("green");
+      return;
+    }
+  });
 
-  cities.forEach((c , i) =>{
-      if(c.innerText.trim().toLowerCase() === value.toLowerCase()){
-          index = i;
-          return;
-        }
-        return;
-      })
-        cities[index].classList.remove("green");
-        console.log(index , "index");        
-        return;
-
-
-
+  tbodyEl.innerHTML = "";
+  cityName.forEach((city) => {
+    fetch(`https://python3-dot-parul-arena-2.appspot.com/test?cityname=${city}`)
+      .then((res) => res.json())
+      .then((data) => {
+        fetchWeather(data, city);
+      });
+  });
 }
 
 function cityWeather(e) {
-  if (!e.target.classList.contains("city")) {
-    return;
-  }
   const cityList = e.target.innerText;
+  highlightCity(cityList);
+
   fetch(
     `https://python3-dot-parul-arena-2.appspot.com/test?cityname=${cityList}`
   )
     .then((res) => res.json())
     .then((data) => {
-        const weatherDate = currentDate - new Date(data.date_and_time);
-          const weatherHours = Math.floor(weatherDate / 3600) %24;
-
-      tbodyEl.innerHTML += `
-        <tr>
-            <td>${cityList}</td>
-            <td>${data.description}</td>
-            <td>${data.temp_in_celsius}</td>
-            <td>${data.pressure_in_hPa}</td>
-            <td>${weatherHours}</td>
-            <td> <button class="deleteBtn"> Delete </button> </td>
-        </tr>
-        `;
-
-      tbodyEl.addEventListener("click", onDeleteRow);
+      fetchWeather(data, cityList);
     });
 }
 
-function fetchAllCities() {
-    let index = -1;
-    cities.forEach((c, i) => {
-        if(c.innerText.toLowerCase() === cityName[i].toLowerCase()){
-            index = i;
-            console.log(cities[index])
-            cities[index].classList.add("green");
-            return;
-        }
-    });
+function fetchWeather(data, value) {
+  const weatherDate = currentDate - new Date(data.date_and_time);
+  const weatherHours = Math.floor(weatherDate / 3600) % 24;
+  const weather = document.createElement("tr");
 
-    tbodyEl.innerHTML ='';
-  cityName.forEach((city) => {
-    fetch(
-      `https://python3-dot-parul-arena-2.appspot.com/test?cityname=${city}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-          const weatherDate = currentDate - new Date(data.date_and_time);
-          const weatherHours = Math.floor(weatherDate / 3600) %24;
-
+  weather.innerHTML = `          
+                <td>${value}</td>
+                <td>${data.description}</td>
+                <td>${data.temp_in_celsius}</td>
+                <td>${data.pressure_in_hPa}</td>
+                <td>${weatherHours}</td>
+                <td> <button class="deleteBtn"> Delete </button> </td>
             
+            `;
+  const deleteBtn = weather.querySelector(".deleteBtn");
+  deleteBtn.addEventListener("click", () => {
+    onDeleteRow(weather, value);
+  });
 
-        tbodyEl.innerHTML += `
-                <tr>
-                    <td>${city}</td>
-                    <td>${data.description}</td>
-                    <td>${data.temp_in_celsius}</td>
-                    <td>${data.pressure_in_hPa}</td>
-                    <td>${weatherHours}</td>
-                    <td> <button class="deleteBtn"> Delete </button> </td>
-                </tr>
-                `;
-        tbodyEl.addEventListener("click",(e) => onDeleteRow(e, index));
-      });
+  tbodyEl.append(weather);
+}
+
+function highlightCity(value) {
+  cities.forEach((c, i) => {
+    if (c.innerText === value) {
+      cities[i].classList.add("green");
+      return;
+    }
   });
 }
-
-
-
-
-
 
 searchBtn.addEventListener("click", getWeather);
 cityEl.addEventListener("click", cityWeather);
 getWeatherBtn.addEventListener("click", fetchAllCities);
-
-
-
